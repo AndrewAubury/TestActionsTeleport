e79652f666a49ad5085b7ee6633a62e0d7a056f3
Andrew Aubury <andrewaubury1@gmail.com>
2024-05-06 14:45:47 +0100

    Change text

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ try {
 // Create the HTTP server
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World Deployed!');
+  res.end('Hello, World Deployed 2!');
 });
 
 // Listen on the Unix socket
